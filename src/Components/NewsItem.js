34fa15e1722d104eb3504fe26c3d346e47157995
Rashika <60001683+rashika10@users.noreply.github.com>
2024-03-02 22:53:30 +0545

    koala d1

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-  render() {
-    return (
-      <div>
-        This is a news item.
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				This is a news item.
+			</div>
+		)
+	}
 }
 
 export default NewsItem
