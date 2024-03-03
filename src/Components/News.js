fe97a55c619e4abb7f7a74bfbf958569443e9040
Rashika <60001683+rashika10@users.noreply.github.com>
2024-03-03 19:24:09 +0545

    koala D2

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,10 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is news component.
-        <NewsItem/>
-
+      <div className="container my-3">
+       <h2>NewsKoala - Top Headlines</h2>
+       <div className="row">
+          <div className="col-md-4">
+          <NewsItem title="myTitle" description="myDesc"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="myTitle" description="myDesc"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="myTitle" description="myDesc"/>
+          </div>
+       </div>
       </div>
     )
   }
