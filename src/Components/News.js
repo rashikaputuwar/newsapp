37d92b57381924cc58e15248206d9a28d8fe512e
Rashika <60001683+rashika10@users.noreply.github.com>
2024-03-06 22:41:51 +0545

    d4

--- src/Components/News.js
+++ src/Components/News.js
@@ -212,6 +212,16 @@ export class News extends Component {
         loading: false
     }
 	}
+
+  async componentDidMount(){
+    console.log("cdm");
+    let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=d00868e2c02d4760959fd2b19f6227f1";
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parseData);
+    this.setState({articles: parsedData.articles})
+  }
+
   render() {
     return (
       <div className="container my-3">
@@ -220,7 +230,7 @@ export class News extends Component {
        <div className="row">
        {this.state.articles.map((element)=>{
          return <div className="col-md-4" key={element.url}>
-          <NewsItem  title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+          <NewsItem  title={!element.title?element.title.slice(0, 45):""} description={element?element.description.slice(0, 88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
           </div>
         })}
        </div>
