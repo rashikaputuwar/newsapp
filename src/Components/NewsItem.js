0d37e58f629287d38e88a449b24c7f7aeab15437
Rashika <60001683+rashika10@users.noreply.github.com>
2024-04-04 17:40:37 +0545

    28- next and previous buttons

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -7,11 +7,12 @@ export class NewsItem extends Component {
 		return (
 			<div className="my-3">
 				<div  className="card" style={{width:"18rem"}}>
-					<img src={imageUrl}  className="card-img-top" alt="..."/>
+					<img src={!imageUrl?"https://techcrunch.com/wp-content/uploads/2024/04/GettyImages-1473057239-e1712083727562.jpg?resize=1200,674":imageUrl} 
+					 className="card-img-top" alt="..."/>
 					<div  className="card-body">
 						<h5  className="card-title">{title}</h5>
 						<p  className="card-text">{description}...</p>
-						<a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+						<a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
 					</div>
 				</div> 
 			</div>
